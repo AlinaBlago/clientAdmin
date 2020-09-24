478e17ec2af93d2f87e981ed13772124a9796946
Alina <alinablago19@gmail.com>
2020-09-24 13:52:45 +0300

    Fixed class LogInController - added logger

--- src/main/java/controller/ApplicationController.java
+++ src/main/java/controller/ApplicationController.java
@@ -1,4 +1,4 @@
 package controller;
 
-public class ApplicationController {
+public interface ApplicationController {
 }
--- src/main/java/controller/LogInController.java
+++ src/main/java/controller/LogInController.java
@@ -1,4 +1,9 @@
 package controller;
 
-public interface LogInController {
+import javafx.fxml.Initializable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public interface LogInController extends Initializable {
+    Logger logger = LoggerFactory.getLogger(LogInController.class);
 }
