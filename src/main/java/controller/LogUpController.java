c89b3ef5c6241379d03bd1bc41a3c6e47bfb8da3
Alina <alinablago19@gmail.com>
2020-09-24 13:53:25 +0300

    Fixed class LogUpController - added logger

--- src/main/java/controller/LogUpController.java
+++ src/main/java/controller/LogUpController.java
@@ -1,4 +1,9 @@
 package controller;
 
-public interface LogUpController {
+import javafx.fxml.Initializable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public interface LogUpController extends Initializable {
+    Logger logger = LoggerFactory.getLogger(LogInController.class);
 }
